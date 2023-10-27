8bee26d9a1aca9e48b0e69cb705e168478627b48
Adam Fragkiadakis <adamfrag@gmail.com>
2023-10-27 15:56:43 +0200

    SVM exercise

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -10,8 +10,9 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
+from tools.email_preprocess import preprocess
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -22,7 +23,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+clf = svm.SVC(kernel="rbf", C=10000)
 
 #########################################################
 
@@ -35,5 +36,17 @@ The Code Below solves that issue, So use this one
 
 # features_train = features_train[:int(len(features_train)/100)]
 # labels_train = labels_train[:int(len(labels_train)/100)]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+accuracy = accuracy_score(pred, labels_test)
+print(f"Accuracy: {accuracy}")
+print(pred[10], pred[26], pred[50])
+print(list(pred).count(1))
 
 #########################################################
